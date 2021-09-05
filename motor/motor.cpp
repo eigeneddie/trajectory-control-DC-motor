036b37d4c6b19b43ffc27a83158f88b9afdf91b2
Eddie <66312407+eigeneddie@users.noreply.github.com>
2021-09-05 12:33:24 +0700

    added motor

---
+++ motor/motor.cpp
@@ -0,0 +1,18 @@
+#include "motor.h"
+
+motor::motor(PinName dright, PinName dleft, PinName pwmpin): dright_(dright), dleft_(dleft), pwmpin_(pwmpin) {
+    pwmpin_.period_us(50);  
+}
+
+void motor::setpwm(float inputpwm) {
+    
+    if (inputpwm >= 0) {
+        dright_ = 1;
+        dleft_ = 0;
+        pwmpin_.write(inputpwm);
+    } else {
+        dright_ = 0;
+        dleft_ = 1;
+        pwmpin_.write(-1 * inputpwm);
+    }  
+}
